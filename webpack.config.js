68924da33acf9f2add39b55039d6a3be602de6e4
Charlie <charliehu2808@outlook.com>
2024-07-28 15:52:52 +0200

    step 3

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HthmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ]
+};
